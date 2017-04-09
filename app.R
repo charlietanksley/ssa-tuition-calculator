6413a9485118a4559c3c152fdeefe4d671228d22
Charlie Tanksley <charlie.tanksley@gmail.com>
2017-04-09 11:24:55 -0400

    works for 2+ kids 5 days with 10% discount

--- app.R
+++ app.R
@@ -12,13 +12,13 @@ library(dplyr)
 
 tuition_data = data.frame(
   income = c(10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000, 110000, 120000, 130000, 140000),
-  price = c(180, 360, 720, 1260, 1980, 2880, 3960, 5220, 6660, 8280, 10080, 12060, 14220, 15000),
+  price = c(180, 360, 720, 1260, 1980, 2880, 3960, 5220, 6660, 8280, 10080, 12060, 14220, 16000),
   income_range = c('Under 20,000', '20,000 - 29,999', '30,000 - 39,999', '40,000 - 49,999', '50,000 - 59,999', '60,000 - 69,999', '70,000 - 79,999', '80,000 - 89,999', '90,000 - 99,999', '100,000 - 109,999', '110,000 - 119,999', '120,000 - 129,999', '130,000 - 139,999', '140,000+'))
 
 tuition_data = mutate(tuition_data,
                       base_per_day = price / 5)
 
-tuition_cap = data.frame(
+tuition_cap_data = data.frame(
   days = c(1, 2, 3, 4, 5),
   cap = c(1, 1, 1, 1, 8000)
 )
@@ -61,9 +61,13 @@ server <- function(input, output) {
     tuition = filter(tuition_data, income_range == input$income)$price
   }
   
+  tuition_cap <- function() {
+    filter(tuition_cap_data, days == input$days)$cap
+  }
+  
   single_tuition <- function() {
     tuition = tuition()
-    cap = filter(tuition_cap, days == input$days)$cap
+    cap = tuition_cap()
     
     if (tuition > cap) {
       return(cap)
@@ -73,7 +77,12 @@ server <- function(input, output) {
   }
   
   additional_tuition <- function() {
-    tuition() / 2
+    tuition = tuition() / 2
+    cap = tuition_cap()
+    if (tuition > cap) {
+      return(cap)
+    }
+    tuition
   }
   
   calculate_tuition <- reactive({
@@ -81,6 +90,14 @@ server <- function(input, output) {
     if (input$children > 1) {
       for (i in 2:input$children) {
         total_tuition = total_tuition + additional_tuition()
+      }
+      
+      cap = tuition_cap()
+      if (total_tuition == cap * input$children) {
+        discount = cap * .1
+        for (i in 2:input$children) {
+          total_tuition = total_tuition - discount
+        }
       }
     }
     #cap_tuition(total_tuition)
