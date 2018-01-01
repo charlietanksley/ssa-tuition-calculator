fb35955b46ccf795c696b53b1a431e5648a171cd
Charlie Tanksley <charlie.tanksley@gmail.com>
2018-01-01 10:00:23 -0500

    this is starting to work some but it is a mess
    In particular, the calculation is off by just a bit...

--- vue-app.js
+++ vue-app.js
@@ -77,6 +77,7 @@ var tuition_adjustments = {
 var four_year_multiplier = 0.688;
 
 function TuitionCalculator(days, daily_tuition, tuition_cap, multiplier, children) {
+    this.sibling_discount = 0.10;
     this.days = days;
     this.daily_tuition = daily_tuition;
     this.tuition_cap = tuition_cap;
@@ -84,18 +85,80 @@ function TuitionCalculator(days, daily_tuition, tuition_cap, multiplier, childre
     this.children = children;
 }
 
-TuitionCalculator.prototype.tuition = function() {
-    // calculate tuition for one kid
+TuitionCalculator.prototype.per_child_tuition = function() {
+    return this.daily_tuition * this.days;
+}
 
-    // Cap the tuition if needed.
-    var tuition = this.daily_tuition * this.days,
+TuitionCalculator.prototype.family_tuition_cap = function() {
+    return this.tuition_cap * this.children;
+}
+
+TuitionCalculator.prototype.cap_tuition = function(family_tuition) {
+    var family_tuition_cap = this.family_tuition_cap();
+
+    console.debug(family_tuition);
+    if (family_tuition > family_tuition_cap) {
+        return family_tuition_cap;
+    } else {
+        return family_tuition;
+    }
+}
+
+TuitionCalculator.prototype.capped_per_child_tuition = function() {
+    var tuition = this.per_child_tuition(),
         tuition_cap = this.tuition_cap;
 
     if (tuition > tuition_cap) {
-        tuition = tuition_cap;
+        return tuition_cap;
+    } else {
+        return tuition;
+    }
+}
+
+TuitionCalculator.prototype.subtract_family_discount = function(family_tuition) {
+    var discount,
+        siblings = this.children - 1,
+        family_tuition_cap = this.family_tuition_cap();
+
+    // If there is only one child OR the family is already getting
+    // tuition assistance, don't add in a sibling discount.
+    if (siblings === 0 || family_tuition !== family_tuition_cap) {
+        return family_tuition;
     }
 
-    return tuition
+    discount = this.capped_per_child_tuition() * this.sibling_discount * siblings;
+    return family_tuition - discount;
+}
+
+TuitionCalculator.prototype.calculate_family_tuition = function(per_child_tuition) {
+    var siblings = this.children - 1,
+        first_child = per_child_tuition,
+        // Okay wow. So there is a bug here wherein the siblings are
+        // paying 1/2 of the base tuition (which can go over 8K per
+        // year). Or something. Somethign is still wrong here.
+        sibling_tuition = per_child_tuition / 2 * siblings;
+
+    return first_child + sibling_tuition;
+}
+
+TuitionCalculator.prototype.scale_for_part_time = function(family_tuition) {
+    return family_tuition * this.multiplier;
+}
+
+TuitionCalculator.prototype.convert_tuition_to_monthly = function(family_tuition) {
+    return (family_tuition / 10).toFixed();
+}
+
+TuitionCalculator.prototype.calculate = function() {
+    return R.pipe(this.calculate_family_tuition.bind(this),
+                  this.scale_for_part_time.bind(this),
+                  this.cap_tuition.bind(this),
+                  this.subtract_family_discount.bind(this),
+                  this.convert_tuition_to_monthly.bind(this));
+}
+
+TuitionCalculator.prototype.tuition = function() {
+    return this.calculate()(this.per_child_tuition());
 }
 
 
