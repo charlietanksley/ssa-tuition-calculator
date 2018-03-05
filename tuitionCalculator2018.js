898f613777e5d2c14014aea9d251d5d2b145d586
Charlie Tanksley <charlie.tanksley@gmail.com>
2018-03-05 08:26:53 -0500

    adds 2018/2019 draft

---
+++ tuitionCalculator2018.js
@@ -0,0 +1,212 @@
+tuition_adjustments = {
+    'five-plus': {
+        1: {
+            'tuition_cap': 2000,
+            'percent_increase': 1.25
+        },
+        2: {
+            'tuition_cap': 3840,
+            'percent_increase': 1.20
+        },
+        3: {
+            'tuition_cap': 5520,
+            'percent_increase': 1.15
+        },
+        4: {
+            'tuition_cap': 7040,
+            'percent_increase': 1.10
+        },
+        5: {
+            'tuition_cap': 8000,
+            'percent_increase': 1.00
+        }
+    },
+    'four-year': {
+        1: {
+            'tuition_cap': 1540,
+            'percent_increase': 1.4
+        },
+        2: {
+            'tuition_cap': 2869,
+            'percent_increase': 1.3
+        },
+        3: {
+            'tuition_cap': 3960,
+            'percent_increase': 1.2
+        },
+        4: {
+            'tuition_cap': 4840,
+            'percent_increase': 1.1
+        },
+        5: {
+            'tuition_cap': 5500,
+            'percent_increase': 1.0
+        }
+    }
+};
+
+four_year_multiplier = 0.688;
+
+function TuitionCalculator(days, income, children, program) {
+    this.sibling_discount = 0.10;
+    this.days = days;
+    this.income = income;
+    this.children = parseInt(children);
+    this.program = program;
+}
+
+
+TuitionCalculator.prototype.full_yearly_tuition_single = function() {
+    var adjustment,
+        tuition_cap,
+        tuition,
+        percent_increase,
+        multiplier,
+        siblings;
+
+    // Adjust tuition for program and days
+    adjustment = tuition_adjustments[this.program][this.days];
+    tuition_cap = adjustment['tuition_cap'];
+    percent_increase = adjustment['percent_increase'];
+    if (this.program == 'five-plus') {
+        multiplier = 1;
+    } else {
+        multiplier = four_year_multiplier;
+    }
+
+    tuition = this.full_price_tuition(percent_increase, tuition_cap) * multiplier;
+
+    // Split into 10 months
+    tuition = Math.round(tuition / 10);
+
+    // Adjust for number of children
+    siblings = this.children - 1;
+    if (siblings > 0) {
+        tuition = tuition + (tuition * siblings * 0.5);
+    }
+
+    return tuition;
+}
+
+TuitionCalculator.prototype.full_price_tuition = function(adjustment, tuition_cap) {
+    var tuition = this.income ** 1.98 + 180 - this.income
+    var adjusted_tuition = tuition * adjustment * (this.days / 5)
+    if (adjusted_tuition > tuition_cap) {
+        adjusted_tuition = tuition_cap;
+    }
+
+    return adjusted_tuition;
+}
+
+TuitionCalculator.prototype.full_tuition = function() {
+    var adjustment,
+        tuition_cap,
+        percent_increase,
+        multiplier;
+
+    // Adjust tuition for program and days
+    adjustment = tuition_adjustments[this.program][this.days];
+    tuition_cap = adjustment['tuition_cap'];
+    percent_increase = adjustment['percent_increase'];
+
+    if (this.program == 'five-plus') {
+        multiplier = 1;
+    } else {
+        multiplier = four_year_multiplier;
+    }
+
+    var adjusted_tuition = 8000 * multiplier * percent_increase * (this.days / 5);
+
+    return Math.round((adjusted_tuition * this.children) / 10);
+}
+
+
+
+
+TuitionCalculator.prototype.calculate = function() {
+    return R.pipe(this.calculate_family_tuition.bind(this),
+                  this.cap_tuition.bind(this),
+                  this.convert_tuition_to_monthly.bind(this));
+}
+
+TuitionCalculator.prototype.calculate_family_tuition = function() {
+    var siblings = this.children - 1,
+        first_child = this.first_child_tuition(),
+        // Okay wow. So there is a bug here wherein the siblings are
+        // paying 1/2 of the base tuition (which can go over 8K per
+        // year). Or something. Somethign is still wrong here.
+        sibling_tuition = this.sibling_tuition() * siblings;
+
+    return first_child + sibling_tuition;
+}
+
+TuitionCalculator.prototype.capped_per_child_tuition = function() {
+    var tuition = this.per_child_tuition(),
+        tuition_cap = this.tuition_cap;
+
+    if (tuition > tuition_cap) {
+        return tuition_cap;
+    } else {
+        return tuition;
+    }
+}
+
+TuitionCalculator.prototype.cap_tuition = function(family_tuition) {
+    var family_tuition_cap = this.family_tuition_cap();
+
+    if (family_tuition > family_tuition_cap) {
+        return family_tuition_cap;
+    } else {
+        return family_tuition;
+    }
+}
+
+TuitionCalculator.prototype.convert_tuition_to_monthly = function(family_tuition) {
+    return (family_tuition / 10).toFixed();
+}
+
+TuitionCalculator.prototype.family_tuition_cap = function() {
+    return this.tuition_cap * this.children;
+}
+
+TuitionCalculator.prototype.first_child_tuition = function() {
+    return this.capped_per_child_tuition();
+}
+
+
+TuitionCalculator.prototype.per_child_tuition = function() {
+    return this.daily_tuition * this.days * this.multiplier;
+}
+
+TuitionCalculator.prototype.sibling_tuition = function() {
+    return this.per_child_tuition() / 2;
+}
+
+TuitionCalculator.prototype.scale_for_part_time = function(family_tuition) {
+    return family_tuition * this.multiplier;
+}
+
+TuitionCalculator.prototype.subtract_family_discount = function(family_tuition) {
+    var discount,
+        siblings = this.children - 1,
+        family_tuition_cap = this.family_tuition_cap();
+
+    // If there is only one child OR the family is already getting
+    // tuition assistance, don't add in a sibling discount.
+    //
+    // So this is also a bug: we give the sibling discount *even if*
+    // they are getting tuition assistance.  Maybe that is good, but
+    // that isn't how I understood it before I started this rewrite.
+    //
+    if (siblings === 0) {// || family_tuition !== family_tuition_cap) {
+        return family_tuition;
+    }
+
+    discount = this.per_child_tuition() * this.sibling_discount * siblings;
+    return family_tuition - discount;
+}
+
+TuitionCalculator.prototype.tuition = function() {
+    // return this.calculate()();
+    return this.full_yearly_tuition_single()
+}
---
+++ vue-app-2018.js
@@ -0,0 +1,88 @@
+var income_map = {
+    "Under 10,000": 1,
+    "10,000 - 14,999": 10,
+    "15,000 - 19,999": 15,
+    "20,000 - 24,999": 20,
+    "25,000 - 29,999": 25,
+    "30,000 - 34,999": 30,
+    "35,000 - 39,999": 35,
+    "40,000 - 44,999": 40,
+    "45,000 - 49,999": 45,
+    "50,000 - 54,999": 50,
+    "55,000 - 59,999": 55,
+    "60,000 - 64,999": 60,
+    "65,000 - 69,999": 65,
+    "70,000 - 74,999": 70,
+    "75,000 - 79,999": 75,
+    "80,000 - 84,999": 80,
+    "85,000 - 89,999": 85,
+    "90,000 - 94,999": 90,
+    "95,000 - 99,999": 95,
+    "100,000 - 104,999": 100,
+    "105,000 - 109,999": 105,
+    "110,000 - 114,999": 110,
+    "115,000 - 119,999": 115,
+    "120,000 - 124,999": 120,
+    "125,000 - 129,999": 125,
+    "130,000+": 130
+};
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        base_tuition: income_map,
+        children: 1,
+        days: 5,
+        income: '95,000 - 99,999',
+        program: 'five-plus',
+        tuition_adjustments: tuition_adjustments
+    },
+    computed: {
+        calculator: function() {
+            var income = income_map[this.income];
+
+            return new TuitionCalculator(
+                this.days,
+                income,
+                this.children,
+                this.program
+            );
+        },
+
+        children_display: function() {
+            if (this.children === 1) {
+                return this.children + " child";
+            } else {
+                return this.children + " children";
+            }
+        },
+
+        days_display: function() {
+            if (this.days === 1) {
+                return this.days + " day";
+            } else {
+                return this.days + " days";
+            }
+        },
+
+        full_tuition: function() {
+            return this.calculator.full_tuition();
+        },
+
+        incomes: function() {
+            return Object.keys(income_map);
+        },
+
+        possible_days: function() {
+            return [5, 4, 3, 2, 1];
+        },
+
+        tuition: function() {
+            return this.calculator.tuition();
+        },
+
+        tuition_assistance: function() {
+            return this.full_tuition - this.tuition;
+        }
+    }
+});
